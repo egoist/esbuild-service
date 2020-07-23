de76504949262a3a2ade6e5371befacd56e151a2
featherlight <zhangcong1992@gmail.com>
2020-07-23 10:48:54 +0800

    feat: redirect to url with versions, fix #13 (#14)

--- api/build.go
+++ api/build.go
@@ -1,13 +1,16 @@
 package api
 
 import (
+	"fmt"
 	"strings"
 
-	"github.com/egoist/esbuild-service/util"
-
 	"github.com/egoist/esbuild-service/builder"
+	"github.com/egoist/esbuild-service/logger"
+	"github.com/egoist/esbuild-service/util"
 	"github.com/gin-gonic/gin"
 )
+
+var log = logger.Logger
 
 func respError(c *gin.Context, status int, err error) {
 	c.JSON(status, gin.H{
@@ -31,12 +34,35 @@ func CreateBuildHandler(b *builder.Builder) gin.HandlerFunc {
 			enableMinify = util.StrToBool(Minify)
 		}
 
+		parsedPkg := util.ParsePkgName(Pkg)
+		version := parsedPkg[2]
+		if version == "" {
+			version = "latest"
+		}
+		pkgVersion, err := builder.GetPkgMatchVersion(parsedPkg[0], version)
+		if err != nil {
+			log.Errorf("failed to get pkg match version: %s, error: %s", Pkg, err.Error())
+			respError(c, 400, err)
+			return
+		}
+
+		// redirect to exact matched version
+		if pkgVersion != version {
+			matchedPkg := fmt.Sprintf("%s%s@%s", parsedPkg[0], parsedPkg[1], pkgVersion)
+			url := strings.Replace(c.Request.URL.String(), Pkg, matchedPkg, 1)
+			log.Infof("redirect to %s", url)
+			c.Redirect(302, url)
+			return
+		}
+
 		content, err := b.Build(&builder.BuildOptions{
 			Pkg:        Pkg,
 			GlobalName: GlobalName,
 			Format:     Format,
 			Platform:   Platform,
 			IsMinify:   enableMinify,
+			ParsedPkg:  parsedPkg,
+			PkgVersion: pkgVersion,
 		}, isForce)
 
 		if err != nil {
